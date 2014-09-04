f3011b749c2075a6c6f22530724fe2a13940da59
Mark Bennett <mwbennett@wesleyan.edu>
2014-09-04 19:28:48 -0400

    testing js issues

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,35 +1,35 @@
 
-$(function() {
-  $('.activities').on('click', function(){
-    $('.activity-box').show();
-    $('.event-box').hide();
-    $('.invite-box').hide();
-  });
-})
+// $(function() {
+//   $('.activities').on('click', function(){
+//     $('.activity-box').show();
+//     $('.event-box').hide();
+//     $('.invite-box').hide();
+//   });
+// })
 
-$(function() {
-  $('.events').on('click', function(){
-    $('.activity-box').hide();
-    $('.event-box').show();
-    $('.invite-box').hide();
-  });
-})
+// $(function() {
+//   $('.events').on('click', function(){
+//     $('.activity-box').hide();
+//     $('.event-box').show();
+//     $('.invite-box').hide();
+//   });
+// })
 
-$(function() {
-  $('.invites').on('click', function(){
-    $('.activity-box').hide();
-    $('.event-box').hide();
-    $('.invite-box').show();
-  });
-})
+// $(function() {
+//   $('.invites').on('click', function(){
+//     $('.activity-box').hide();
+//     $('.event-box').hide();
+//     $('.invite-box').show();
+//   });
+// })
 
-$(".alert").alert()
+// $(".alert").alert()
 
-$('ul.nav-pills li a').on('click', function () {
-  $('ul.nav-pills li.active').removeClass('active')
-  $(this).parent('li').addClass('active')
-})
+// $('ul.nav-pills li a').on('click', function () {
+//   $('ul.nav-pills li.active').removeClass('active')
+//   $(this).parent('li').addClass('active')
+// })
 
-$(function () {
-  $('#datetimepicker1').datetimepicker()
-});
+// $(function () {
+//   $('#datetimepicker1').datetimepicker()
+// });
